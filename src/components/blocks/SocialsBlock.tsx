4448cb8da91a41c157256819b3f00d47c6712118
Brian Rahadi <brian.rahadi@gmail.com>
2026-01-25 17:12:55 -0800

    fix: SocialsBloxk short in mobile

--- src/components/blocks/SocialsBlock.tsx
+++ src/components/blocks/SocialsBlock.tsx
@@ -9,7 +9,7 @@ interface BlockProps {
 
 export function SocialsBlock({ colSpan = 1, rowSpan = 1 }: BlockProps) {
     return (
-        <BentoItem colSpan={colSpan} rowSpan={rowSpan} className="h-64 md:h-auto flex flex-col justify-center items-center gap-4 bg-[#fefae0] !p-0">
+        <BentoItem colSpan={colSpan} rowSpan={rowSpan} className="h-32 md:h-auto flex flex-col justify-center items-center gap-4 bg-[#fefae0] !p-0">
             <div className="grid grid-cols-2 gap-2 w-full h-full p-4">
                 <a href="#" className="border border-[#E7E5E4] flex items-center justify-center bg-white rounded-2xl hover:scale-105 transition-transform text-stone-600 shadow-sm hover:shadow-md hover:text-stone-900">
                     <FileText size={24} />
