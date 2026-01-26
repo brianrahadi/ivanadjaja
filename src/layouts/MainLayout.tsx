263a131dd48900a73b7ffd8105105c562a3dcd72
Brian Rahadi <brian.rahadi@gmail.com>
2026-01-25 20:17:31 -0800

    fix: footer fix layout

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -3,13 +3,13 @@ import { Header } from '../components/Header';
 
 export function MainLayout({ children }: { children: ReactNode }) {
     return (
-        <div className="min-h-screen bg-[#ccd5ae] text-stone-800 font-pica selection:bg-rose-100 relative flex flex-col items-center p-4 md:p-8">
+        <div className="min-h-screen bg-[#ccd5ae] text-stone-800 font-pica selection:bg-rose-100 relative flex flex-col items-center p-4">
             <Header />
             <div className="w-full">
                 {children}
             </div>
 
-            <footer className="mt-12 mb-4 text-center text-stone-500 text-sm font-pica opacity-60 hover:opacity-100 transition-opacity">
+            <footer className="mt-12 mb-2 text-center text-stone-500 text-sm font-pica opacity-60 hover:opacity-100 transition-opacity">
                 <a href="https://brianrahadi.com" target="_blank" rel="noopener noreferrer" className="hover:underline">
                     built with &lt;3 by brianrahadi
                 </a>
